d49e3c7829cdb75ed2a85247fb7ed535dcba66bc
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2019-01-27 19:59:18 +0900

    タイトルシーンを追加

---
+++ FlappyCube/Assets/Script/SceneManager/GameStarter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStarter : MonoBehaviour
+{
+    GameObject manager = null;
+
+    void Start()
+    {
+        manager = GameObject.Find("SceneManager");   
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        var o_w = Screen.width / 2;
+        var o_h = Screen.height / 2;
+        var b_width = 100;
+        var b_height = 50;
+        if (GUI.Button(new Rect(o_w - b_width/2, o_h - b_height/2, b_width, b_height), "Start"))
+        {
+            manager.GetComponent<StageLoader>().enabled = true;
+            SceneManager.UnloadSceneAsync(gameObject.scene.name);
+        }
+    }
+}
---
+++ FlappyCube/Assets/Script/SceneManager/TitleChecker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleChecker : MonoBehaviour
+{
+    void Start()
+    {
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            //既にタイトルシーンが読み込まれているかを確認する
+            var name = SceneManager.GetSceneAt(i).name;
+            if (name == "Title")
+            {
+                return;
+            }
+        }
+
+        GetComponent<StageLoader>().enabled = true;
+    }
+}
