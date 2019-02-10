ab67d05ff8e4e35d4c8db6cb1efecef918a6b974
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2019-02-10 20:36:26 +0900

    メッシュトポロジーを変更するスクリプト作成

---
+++ FlappyCube/Assets/Script/Graphics/MeshTopologyChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MeshTopologyChanger : MonoBehaviour
+{
+    [SerializeField]
+    MeshTopology meshTopology = MeshTopology.Triangles;
+
+    MeshFilter meshFilter;
+
+    void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        meshFilter.mesh.SetIndices(meshFilter.mesh.GetIndices(0), meshTopology, 0);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnValidate()
+    {
+        meshFilter.mesh.SetIndices(meshFilter.mesh.GetIndices(0), meshTopology, 0);
+    }
+}
